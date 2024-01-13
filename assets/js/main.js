859a47174eb1624d634606971a95c4ea4fd2bcc9
Ben <>
2024-01-13 18:13:51 +0300

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,7 +124,6 @@ sr.reveal(`.home__data, .home__social-link, .home__info,
            .discover__container,
            .experience__data, .experience__overlay,
            .place__card,
-           .sponsor__content,
            .footer__data, .footer__rights`,{
     origin: 'top',
     interval: 100,
